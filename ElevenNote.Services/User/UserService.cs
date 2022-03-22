6040fc3e767722e342084a127bf621feb38cdfdc
Eilers Fam <alexeilers@Alexs-iMac.local>
2022-03-22 10:57:37 -0400

    added password hasher and GetUserByID method

--- ElevenNote.Services/User/UserService.cs
+++ ElevenNote.Services/User/UserService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using ElevenNote.Data;
 using ElevenNote.Data.Entities;
 using ElevenNote.Models.User;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace ElevenNote.Services.User
@@ -25,14 +26,34 @@ namespace ElevenNote.Services.User
             {
                 Email = model.Email,
                 Username = model.Username,
-                Password = model.Password,
                 DateCreated = DateTime.Now
             };
+
+            var passwordHasher = new PasswordHasher<UserEntity>();
+            entity.Password = passwordHasher.HashPassword(entity, model.Password);
 
             _context.Users.Add(entity);
             var numberOfChanges = await _context.SaveChangesAsync();
 
             return numberOfChanges == 1;
+        }
+
+        public async Task<UserDetail> GetUserByIdAsync(int userId)
+        {
+            var entity = await _context.Users.FindAsync(userId);
+            if (entity is null)
+                return null;
+
+            var userDetail = new UserDetail{
+                Id = entity.Id,
+                Email = entity.Email,
+                Username = entity.Username,
+                FirstName = entity.FirstName,
+                LastName = entity.LastName,
+                DateCreated = entity.DateCreated
+            };
+
+            return userDetail;
         }
 
         private async Task<UserEntity> GetUserByEmailAsync(string email)
