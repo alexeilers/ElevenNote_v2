26dc0517ff7a2ce58fd3c42def1dd079469b7c9a
Eilers Fam <alexeilers@Alexs-iMac.local>
2022-03-22 14:04:05 -0400

    added note controller

--- ElevenNote.WebAPI/Controllers/NoteController.cs
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ElevenNote.Services.Note;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ElevenNote.WebAPI.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class NoteController : ControllerBase
@@ -17,6 +19,13 @@ namespace ElevenNote.WebAPI.Controllers
         public NoteController(INoteService noteservice)
         {
             _noteService = noteservice;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllNotes()
+        {
+            var notes = await _noteService.GetAllNotesAsync();
+            return Ok(notes);
         }
     }
 }
